e3ee44aa7880ed2419c372fd833fbd1814849f89
Ken Luo <luozhirong@gmail.com>
2018-10-13 14:54:07 -0700

    added more material ui stuffs

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {InstantSearch} from 'react-instantsearch-dom';
 import Search from './component/searchBar';
-import Cocktail from './component/cocktail';
+// import Cocktail from './component/cocktail';
 import Cocktails from './component/cocktails';
 
+
+
+const appId='M3OEEU8RL9';
+const apiKey="a8c912614f3096773cb168c43e710bc0";
+const indexName="bottomsUp";
+
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      drinks: []
+    }
+    
+  }
+
   render() {
     return (
       <div>
-        <Search/>
-        
+        <InstantSearch
+          appId={appId}
+          apiKey={apiKey}
+          indexName={indexName}
+          onSearchStateChange={this.props.onSearchStateChange}
+          >
+            <Search/>
+          </InstantSearch>
         <Cocktails/>
       </div>
     );
