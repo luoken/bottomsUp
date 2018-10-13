2cef941c70f272da7cf228242f13e4c6e040bead
Ken Luo <luozhirong@gmail.com>
2018-10-13 13:55:36 -0700

    added material table

--- src/component/cocktail.js
+++ src/component/cocktail.js
@@ -2,7 +2,14 @@ import React, {Component} from 'react';
 
 
 class Cocktail extends Component {
-
+    render(){
+        return(
+            <div>
+                <p>Title: <span>Jack n Coke</span>
+                 <div>Ingredients: <span>Jack, Coke, Ice</span></div></p>
+            </div>
+        )
+    }
 }
 
 export default Cocktail;
---
+++ src/component/cocktails.js
@@ -0,0 +1,58 @@
+import React, {Component} from 'react';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+let id = 0;
+function createData(name, calories, fat, carbs, protein) {
+    id += 1;
+    return { id, name, calories, fat, carbs, protein };
+  }
+const rows = [
+    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+    createData('Eclair', 262, 16.0, 24, 6.0),
+    createData('Cupcake', 305, 3.7, 67, 4.3),
+    createData('Gingerbread', 356, 16.0, 49, 3.9),
+  ];
+
+function Cocktails(props) {
+    const { classes } = props;
+  
+    return (
+      <Paper>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Dessert (100g serving)</TableCell>
+              <TableCell numeric>Calories</TableCell>
+              <TableCell numeric>Fat (g)</TableCell>
+              <TableCell numeric>Carbs (g)</TableCell>
+              <TableCell numeric>Protein (g)</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map(row => {
+              return (
+                <TableRow key={row.id}>
+                  <TableCell component="th" scope="row">
+                    {row.name}
+                  </TableCell>
+                  <TableCell numeric>{row.calories}</TableCell>
+                  <TableCell numeric>{row.fat}</TableCell>
+                  <TableCell numeric>{row.carbs}</TableCell>
+                  <TableCell numeric>{row.protein}</TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </Paper>
+    );
+  }
+  
+
+export default Cocktails;
