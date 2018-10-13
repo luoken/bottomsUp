ecd2860f58c2daabb1c0047764582ca275679712
Ken Luo <luozhirong@gmail.com>
2018-10-13 14:07:18 -0700

    added bootstrap

--- src/component/cocktails.js
+++ src/component/cocktails.js
@@ -7,16 +7,16 @@ import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 
 let id = 0;
-function createData(name, calories, fat, carbs, protein) {
+function createData(name, type, ingredient1, ingredient2, ingredient3,ingredient4,ingredient5,ingredient6) {
     id += 1;
-    return { id, name, calories, fat, carbs, protein };
+    return { id, name, type, ingredient1, ingredient2, ingredient3,ingredient4,ingredient5,ingredient6};
   }
 const rows = [
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
+    createData('\'57 Chevy with a White License Plate', 'Cocktail', 'Creme de Cacao', 'Vodka'),
+    createData('Ice cream sandwich', 'Shot', 'Absolut Kurant', 'Grand Marnier', 'Chambord raspberry liqueur', 'Midori melon liqueur', 'Malibu rum'),
+    createData('110 in the shade', 'Beer', 'Lager', 'Tequila', '151 proof rum', 'Dark Creme de Cacao','Cointreau'),
+    createData('151 Florida Bushwacker', 'Milk / Float / Shake', 'Malibu rum', 'Light rum', 'Vodka','Orange juice'),
+    createData('155 Belmont', 'Cocktail', 'Dark rum', 'Light rum', 'Rumple Minze','151 proof rum'),
   ];
 
 function Cocktails(props) {
@@ -27,12 +27,15 @@ function Cocktails(props) {
         <Table>
           <TableHead>
             <TableRow>
-              <TableCell>Dessert (100g serving)</TableCell>
-              <TableCell numeric>Calories</TableCell>
-              <TableCell numeric>Fat (g)</TableCell>
-              <TableCell numeric>Carbs (g)</TableCell>
-              <TableCell numeric>Protein (g)</TableCell>
-            </TableRow>
+              <TableCell>Drink</TableCell>
+              <TableCell>Type</TableCell>
+              <TableCell>ingredient1</TableCell>
+              <TableCell>ingredient2</TableCell>
+              <TableCell>ingredient3</TableCell>
+              <TableCell>ingredient4</TableCell>
+              <TableCell>ingredient5</TableCell>
+              <TableCell>ingredient6</TableCell>
+             </TableRow>
           </TableHead>
           <TableBody>
             {rows.map(row => {
@@ -41,10 +44,13 @@ function Cocktails(props) {
                   <TableCell component="th" scope="row">
                     {row.name}
                   </TableCell>
-                  <TableCell numeric>{row.calories}</TableCell>
-                  <TableCell numeric>{row.fat}</TableCell>
-                  <TableCell numeric>{row.carbs}</TableCell>
-                  <TableCell numeric>{row.protein}</TableCell>
+                  <TableCell>{row.type}</TableCell>
+                  <TableCell>{row.ingredient1}</TableCell>
+                  <TableCell>{row.ingredient2}</TableCell>
+                  <TableCell>{row.ingredient3}</TableCell>
+                  <TableCell>{row.ingredient4}</TableCell>
+                  <TableCell>{row.ingredient5}</TableCell>
+                  <TableCell>{row.ingredient6}</TableCell>
                 </TableRow>
               );
             })}
