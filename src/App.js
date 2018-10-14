3448a07a5513b35034dec001152ab0f2025386a1
Ken Luo <luozhirong@gmail.com>
2018-10-13 22:54:11 -0700

    styled

--- src/App.js
+++ src/App.js
@@ -12,17 +12,18 @@ import {
 } from 'react-instantsearch-dom';
 import PropTypes from 'prop-types';
 import './App.css';
+import Cocktails from './component/cocktails';
 
 class App extends Component {
   render() {
     return (
       <div className="bottomsUp-InstantSearch">
-        <h1>Bottoms UP</h1>
+        <h1 style={{paddingLeft: '1%'}}>Bottoms UP</h1>
         <InstantSearch
           appId="M3OEEU8RL9"
           apiKey="a8c912614f3096773cb168c43e710bc0"
           indexName="ingredients_NAME"
-            >
+        >
 
           <div className="left-panel">
             <ClearRefinements />
@@ -31,17 +32,17 @@ class App extends Component {
             <Configure hitsPerPage={8} />
           </div>
             <div className="right-panel">
-	    
-            <SearchBox />
-	    <SortBy
-	defaultRefinement="ingredients_NAME"
-	items={[
-	    {label: "", value: "ingredients_NAME"},
-	    {label: "Name Asc.", value: "ingredients_name_asc"},
-	    {label: "Name Desc.", value: "ingredients_name_desc"}
-	]}
-	    />
-            <Hits hitComponent={Hit} />
+              <div style={{width:'50%'}}>
+                <SearchBox autoFocus='true' style={{width:'50%'}}/>
+              </div>
+                <SortBy
+                  defaultRefinement="ingredients_NAME"
+                  items={[
+                      {label: "Name Asc.", value: "ingredients_NAME"},
+                      {label: "Name Desc.", value: "ingredients_name_desc"}
+                  ]}
+                />
+            <Hits hitComponent={Cocktails} />
             <Pagination />
           </div>
         </InstantSearch>
@@ -50,25 +51,25 @@ class App extends Component {
   }
 }
 
-function Hit(props) {
-  return (
-    <div>
-	  <img src={props.hit.strDrinkThumb} align="left" alt={props.hit.strDrink} />
+// function Hit(props) {
+//   return (
+//     <div>
+// 	  <img src={props.hit.strDrinkThumb} align="left" alt={props.hit.strDrink} />
 	  
-      <div className="hit-name">
-          <Highlight attribute="name" hit={props.hit.strDrink} />{props.hit.strDrink}
-      </div>x
+//       <div className="hit-name">
+//           <Highlight attribute="name" hit={props.hit.strDrink} />{props.hit.strDrink}
+//       </div>x
 	  
-      <div className="hit-description">
-          <Highlight attribute="description" hit={props.hit.strIngredient1} />
-      </div>
-      <div className="hit-price">{props.hit.strIngredient1}</div>
-    </div>
-  );
-}
+//       <div className="hit-description">
+//           <Highlight attribute="description" hit={props.hit.strIngredient1} />
+//       </div>
+//       <div className="hit-price">{props.hit.strIngredient1}</div>
+//     </div>
+//   );
+// }
 
-Hit.propTypes = {
-  hit: PropTypes.object.isRequired,
-};
+// Hit.propTypes = {
+//   hit: PropTypes.object.isRequired,
+// };
 
 export default App;
--- src/component/cocktails.js
+++ src/component/cocktails.js
@@ -2,36 +2,40 @@ import React, {Component} from 'react';
 import 'bulma/css/bulma.css';
 import '../App.css';
 
-function Cocktails(props){
-    return(
-        <div style={{paddingBottom:'10px'}}>
-            <div>
-              <img src={props.hit.strDrinkThumb} alt={props.hit.strDrink} width="40" height="40"/>
-                <span style={{paddingRight:"40px", paddingLeft: "10px"}}>{props.hit.strDrink}</span>
-                <span className="Cocktails-Spacing">{props.hit.strCategory}</span>
+class Cocktails extends Component{
+    render(){
+        return(
+            <div style={{paddingBottom:'10px'}}>
+                <div>
+                <img style={{flexDirection:'row'}}src={this.props.hit.strDrinkThumb} alt={this.props.hit.strDrink} align="left"/>
+                    <div className="hit-name">
+                        <span style={{paddingRight:'20px'}}>{this.props.hit.strDrink}</span>
+                        <span className="Cocktails-Spacing" style={{fontSize:'12px'}}>{this.props.hit.strCategory}</span>
+                    </div>
+                </div>
+                <div>
+                </div>
+                <div style={{fontSize: '12px', flexDirection:'row'}}>
+                    <span style={{paddingRight:"20px"}}><u>Ingredients: </u></span>
+                    <li>{this.props.hit.strIngredient1}</li>
+                    <li>{this.props.hit.strIngredient2}</li>
+                    <li>{this.props.hit.strIngredient3}</li>
+                    <li>{this.props.hit.strIngredient4}</li>
+                    <li>{this.props.hit.strIngredient5}</li>
+                    <li>{this.props.hit.strIngredient6}</li>
+                    <li>{this.props.hit.strIngredient7}</li>
+                    <li style={{paddingLeft:'176px'}}>{this.props.hit.strIngredient8}</li>
+                    <li style={{paddingLeft:'176px'}}>{this.props.hit.strIngredient9}</li>
+                    <li style={{paddingLeft:'176px'}}>{this.props.hit.strIngredient10}</li>
+                    <li style={{paddingLeft:'176px'}}>{this.props.hit.strIngredient11}</li>
+                    <li style={{paddingLeft:'176px'}}>{this.props.hit.strIngredient12}</li>                   
+                    <li style={{paddingLeft:'176px'}}>{this.props.hit.strIngredient13}</li>
+                    <li style={{paddingLeft:'176px'}}>{this.props.hit.strIngredient14}</li>                    
+                    <li style={{paddingLeft:'176px'}}>{this.props.hit.strIngredient15}</li>
+                </div>
             </div>
-            <div>
-            </div>
-            <div style={{fontSize: '12px'}}>
-                <span style={{paddingRight:"20px"}}>Ingredients: </span>
-                <span className="Cocktails-Spacing">{props.hit.strIngredient1}</span>
-                <span className="Cocktails-Spacing">{props.hit.strIngredient2}</span>
-                <span className="Cocktails-Spacing">{props.hit.strIngredient3}</span>
-                <span className="Cocktails-Spacing">{props.hit.strIngredient4}</span>
-                <span className="Cocktails-Spacing">{props.hit.strIngredient5}</span>
-                <span className="Cocktails-Spacing">{props.hit.strIngredient6}</span>
-                <span className="Cocktails-Spacing">{props.hit.strIngredient7}</span>
-                <span className="Cocktails-Spacing">{props.hit.strIngredient8}</span>
-                <span className="Cocktails-Spacing">{props.hit.strIngredient9}</span>
-                <span className="Cocktails-Spacing">{props.hit.strIngredient10}</span>
-                <span className="Cocktails-Spacing">{props.hit.strIngredient11}</span>
-                <span className="Cocktails-Spacing">{props.hit.strIngredient12}</span>
-                <span className="Cocktails-Spacing">{props.hit.strIngredient13}</span>
-                <span className="Cocktails-Spacing">{props.hit.strIngredient14}</span>
-                <span className="Cocktails-Spacing">{props.hit.strIngredient15}</span>
-            </div>
-        </div>
-    )
+        )
+            }
 }  
 
 
