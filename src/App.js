4c9345d42ab72c7987cb928baec1ddb011989684
Danny Padilla <dpadilla010@gmail.com>
2018-10-13 21:58:17 -0700

    new ui

--- src/App.js
+++ src/App.js
@@ -1,57 +1,74 @@
 import React, { Component } from 'react';
+import {
+  InstantSearch,
+  Hits,
+  SearchBox,
+  Pagination,
+  Highlight,
+  SortBy,
+  ClearRefinements,
+  RefinementList,
+  Configure,
+} from 'react-instantsearch-dom';
+import PropTypes from 'prop-types';
 import './App.css';
-import {InstantSearch, Hits, Highlight, Configure, SearchBox,
-	ClearRefinements, RefinementList, SortBy} from 'react-instantsearch-dom';
-// import Search from './component/searchBar';
-// import Cocktail from './component/cocktail';
-import Cocktails from './component/cocktails';
-
-
-
-const appId='M3OEEU8RL9';
-const apiKey="a8c912614f3096773cb168c43e710bc0";
-const indexName="ingredients_NAME";
 
 class App extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      drinks: []
-    }
-
-  }
-
   render() {
-      return (
-	      <div style={{paddingLeft: '20px'}}>
-	      <h1 style={{margin:'5px'}}>Bottoms Up</h1>
-	      <InstantSearch
-          appId={appId}
-          apiKey={apiKey}
-          indexName={indexName}
-          onSearchStateChange={this.props.onSearchStateChange}
-        >
+    return (
+      <div className="bottomsUp-InstantSearch">
+        <h1>Bottoms UP</h1>
+        <InstantSearch
+          appId="M3OEEU8RL9"
+          apiKey="a8c912614f3096773cb168c43e710bc0"
+          indexName="ingredients_NAME"
+            >
+
           <div className="left-panel">
             <ClearRefinements />
-            <h2 style={{margin: '5px'}}>Category</h2>
+            <h2>Category</h2>
             <RefinementList attribute="strCategory" />
-          </div>
-          <div className="right-panel">
             <Configure hitsPerPage={8} />
-            <SearchBox className='input'/>
-            <SortBy
-              defaultRefinement="ingredients_NAME"
-              items={[
-                {label: "Name Asc.", value: "ingredients_NAME"},
-                {label: "Name Desc.", value: "ingredients_name_desc"}
-              ]}
-            />
-            <Hits hitComponent={Cocktails}/>
-	      </div>
+          </div>
+            <div className="right-panel">
+	    
+            <SearchBox />
+	    <SortBy
+	defaultRefinement="ingredients_NAME"
+	items={[
+	    {label: "", value: "ingredients_NAME"},
+	    {label: "Name Asc.", value: "ingredients_name_asc"},
+	    {label: "Name Desc.", value: "ingredients_name_desc"}
+	]}
+	    />
+            <Hits hitComponent={Hit} />
+            <Pagination />
+          </div>
         </InstantSearch>
       </div>
     );
   }
 }
+
+function Hit(props) {
+  return (
+    <div>
+	  <img src={props.hit.strDrinkThumb} align="left" alt={props.hit.strDrink} />
+	  
+      <div className="hit-name">
+          <Highlight attribute="name" hit={props.hit.strDrink} />{props.hit.strDrink}
+      </div>x
+	  
+      <div className="hit-description">
+          <Highlight attribute="description" hit={props.hit.strIngredient1} />
+      </div>
+      <div className="hit-price">{props.hit.strIngredient1}</div>
+    </div>
+  );
+}
+
+Hit.propTypes = {
+  hit: PropTypes.object.isRequired,
+};
 
 export default App;
